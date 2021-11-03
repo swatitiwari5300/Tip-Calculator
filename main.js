e09bea7458b048ebf2ef2e248eccc66bbd060694
swatitiwari5300 <56589754+swatitiwari5300@users.noreply.github.com>
2021-11-03 22:45:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-let btn = document.getElementById('btn');
+window.onload = () =>{
+    document.querySelector('#calculate').onclick = calculateTip;
+}
 
-btn.addEventListener('click', function(){
-  let billAmount =  document.getElementById('bill_amount').value;
-  let tipPercentage =  document.getElementById('tip_percentage').value;
+function calculateTip(){
+    let amount = document.querySelector('#amount').value;
+    let persons = document.querySelector('#persons').value;
+    let service = document.querySelector('#services').value;
 
+    console.log(service);
+    if(amount === '' && service === 'Select'){
+        alert("Please enter valid values");
+        return;
+    }
 
-  let tipAmount = document.getElementById('tip_amount').value = billAmount / tipPercentage;
-  let totalBill = document.getElementById('total_bill').value = parseFloat(billAmount)+parseFloat(tipAmount);
-})
+    if(persons === '1'){
+        document.querySelector('#each').style.display = 'none';
+    }else{
+        document.querySelector('#each').style.display = 'block';
+    }
+
+    let total = (amount * service) / persons;
+    console.log(total);
+    total = Math.round(total * 100)/ 100;
+    total = total.toFixed(2);
+    console.log(total);
+
+    document.querySelector('.tip').style.display = 'block';
+    document.querySelector('#total').innerHTML = total;
+}
