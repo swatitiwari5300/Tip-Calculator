8789486e6dc98d3703f2a9fcb53c446d828dbcb2
swatitiwari5300 <56589754+swatitiwari5300@users.noreply.github.com>
2021-11-03 19:24:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+  let billAmount =  document.getElementById('bill_amount').value;
+  let tipPercentage =  document.getElementById('tip_percentage').value;
+
+
+  let tipAmount = document.getElementById('tip_amount').value = billAmount / tipPercentage;
+  let totalBill = document.getElementById('total_bill').value = parseFloat(billAmount)+parseFloat(tipAmount);
+})
